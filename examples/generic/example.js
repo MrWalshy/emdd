d3e0b2c2c868296b9a08330dd3d162c93477369d
MrWalshyType2 <mwalsh@qa.com>
2023-01-20 00:47:11 +0000

    Added html site generator

---
+++ examples/generic/example.js
@@ -0,0 +1,19 @@
+import { readFileSync, writeFile } from "fs";
+import path from "path";
+import EMDX from "./emdx.js";
+import JSXPlugin, { HtmlDocTypePlugin, JSPlugin, LiteralPlugin, ReactDocTypePlugin } from "./src/plugins.js";
+
+const emdxExample = readFileSync("./example.emdx", "utf-8");
+
+const jsx = new JSXPlugin();
+const js = new JSPlugin();
+const reactTransformPlugin = new ReactDocTypePlugin();
+const htmlTransformPlugin = new HtmlDocTypePlugin();
+const literalPlugin = new LiteralPlugin();
+const emdx = new EMDX([jsx, js, literalPlugin]);
+const output = emdx.transpile(emdxExample, null, htmlTransformPlugin);
+console.log(JSON.stringify(output, null, "  "));
+
+writeFile(path.resolve("./example.jsx"), output, err => {
+    if (err) console.error(err);
+});
---
+++ examples/htmlSiteGenerator/emdx_example_runner.js
@@ -0,0 +1,10 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { EmdxHtmlGenerator } from "../../emdx.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname)
+const generator = new EmdxHtmlGenerator(path.resolve(__dirname, "./config.json"));
+generator.generate();
