aa814f47e60df059090080c3e564d0a1a798d9ae
MrWalshyType2 <mwalsh@qa.com>
2023-01-25 14:34:21 +0000

    Added implementation for deferred JS in full block syntax

--- src/Transpiler.js
+++ src/Transpiler.js
@@ -261,33 +261,61 @@ export class JSTransformer extends ContentTransformerPlugin {
         super("js")
     }
 
+    getArgs(parameters) {
+        const args = {};
+        parameters.forEach(parameter => args[parameter.name] = parameter);
+        return args;
+    }
+
     transform(block) {
-        if (block.type === BlockType.INLINE_PLUGIN) return this.transformInline(block);
+        const args = this.getArgs(block.parameters);
+        if (block.type === BlockType.INLINE_PLUGIN) return this.transformInline(args);
+        return this.transformBlock(block, args);
+    }
+
+    transformBlock(block, args) {
+        if (args.call) {
+            const tryCallResult = this.tryCall(args);
+            if (tryCallResult) return tryCallResult;
+        }
+
+        if (args.name && args.defer && args.defer.value === "true") {
+            this._context[args.name.value] = new Function("context", block.value.value);
+            return "";
+        } else if (args.name) {
+            // if not deferring execution, store a copy of the function and execute
+            this._context[args.name.value] = new Function("context", block.value.value);
+        }
         return Function("context", block.value.value)(this._context) || "";
     }
 
-    transformInline(block) {
-        const callParam = block.parameters.find(param => param.name === "call");
-        if (callParam) try {
-            return this._context[callParam.value](this._context);
-        } catch (error) {
-            console.warn("Function with name '" + callParam.value + "' does not exist in the current context.");
-        }
+    transformInline(args) {
+        this.checkForRequiredInlineArgs(args);
+        const tryCallResult = this.tryCall(args)
+        if (tryCallResult) return tryCallResult;
 
-        const nameParam = block.parameters.find(param => param.name === "name");
-        const deferParam = block.parameters.find(param => param.name === "defer");
-        const valueParam = block.parameters.find(param => param.name === "value");
-        deepLog([callParam, nameParam, deferParam, valueParam])
-        if (!callParam && !valueParam) throw new TranspilerError("JSTransformer: Could not find 'value' or 'call' argument for inline plugin.");
-        if (callParam && valueParam) throw new TranspilerError("JSTransformer: 'call' and 'value' cannot both be specified.");
-        if (nameParam && deferParam && deferParam.value === "true") {
-            this._context[nameParam.value] = new Function("context", valueParam.value);
+        if (args.name && args.defer && args.defer.value === "true") {
+            this._context[args.name.value] = new Function("context", args.value.value);
             return "";
-        } else if (nameParam) {
+        } else if (args.name) {
             // if not deferring execution, store a copy of the function and execute
-            this._context[nameParam.value] = new Function("context", valueParam.value);
+            this._context[args.name.value] = new Function("context", args.value.value);
         }
-        return Function("context", valueParam.value)(this._context) || "";
+        return Function("context", args.value.value)(this._context) || "";
+    }
+
+    checkForRequiredInlineArgs(args) {
+        if (!args.call && !args.value) throw new TranspilerError("JSTransformer: Could not find 'value' or 'call' argument for inline plugin.");
+        if (args.call && args.value) throw new TranspilerError("JSTransformer: 'call' and 'value' cannot both be specified.");
+    }
+
+    tryCall(args) {
+        if (args.call) try {
+            return this._context[args.call.value](this._context);
+        } catch (error) {
+            console.warn("Function with name '" + args.call.value + "' does not exist in the current context.");
+        }
+        return null;
     }
 }
 
